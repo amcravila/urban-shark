21cd3c75e9bf484dcbba99922f20511c54f6693a
amcravila <amcravila@gmail.com>
2018-10-04 18:40:52 -0300

    tela inicial

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   let movies = getMoviesInDB();
 });
 
-  function getMoviesInDB() {
+function getMoviesInDB() {
   database.ref('/partners/').once('value')
   .then(function(snapshot){
     let movies = []
@@ -11,25 +11,31 @@ $(document).ready(function(){
       var childData = childSnapshot.val();
       childSnapshot.forEach(function(d) {
         movies.push(d.val())
-        // return movies;
-})
-// function getMoviesInDB()
       })
-      for (var valor of movies) {
-        let image = valor.image;
-        let title = valor.title;
-        let discount = valor.discount;
-        $('#coupons').append(
-        `<div class="card" style="width: 18rem;">
-         <img class="card-img-top" src="${image}" alt="Card image cap">
-         <div class="card-body d-flex justify-content-between">
-           <h5 class="card-title">${title}</h5>
-           <h5 class="card-title">${discount}</h5>
-           <button type="button" class="btn btn-secondary">Reserve</button>
-         </div>
-        </div>
-        `);
-}
-
+    })
+    for (var valor of movies) {
+      let image = valor.image;
+      let title = valor.title;
+      let discount = valor.discount;
+      $('#coupons').append(
+      `<div class="card" style="width: 18rem;">
+       <img class="card-img-top" src="${image}" alt="Card image cap">
+       <div class="card-body d-flex justify-content-between">
+         <h5 class="card-title">${title}</h5>
+         <h5 class="card-title">${discount}</h5>
+         <button type="button" data-id="${valor}" class="btn btn-secondary btnList">Reserve</button>
+       </div>
+      </div>
+      `);
+      $(`.btnList[data-id="${valor}"]`).on("click", () => {
+        localStorage.setItem('userCupon', valor);
+        localStorage.setItem('image', valor.image);
+        localStorage.setItem('cupons', valor.coupons);
+        localStorage.setItem('title', valor.title);
+        localStorage.setItem('description', valor.description);
+        localStorage.setItem('percent', valor.percent);
+        window.location = "ok.html";
       })
     }
+  })
+}
