ad05baeda0cd6d4f661966536c4ef40b997195da
Thalissa Jenniffer <thalissajenniffer@hotmail.com>
2018-10-04 12:16:00 -0300

    links e estrutura

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#reserve").click(updateReserve)
+
+})
+
+
+//atualizar cupons no firebase
+//atualizar numero de cupons na tela
+function updateReserve() {
+  getCuponsDB()
+}
+
+function getCuponsDB() {
+
+}
