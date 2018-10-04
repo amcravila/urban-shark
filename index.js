680b8cbf653c54a42537d9ba61b9856a965a26f6
amcravila <amcravila@gmail.com>
2018-10-04 18:25:06 -0300

    tela inicial

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 $(document).ready(function(){
   let movies = getMoviesInDB();
-  console.log(movies);
-})
-function getMoviesInDB() {
+});
+
+  function getMoviesInDB() {
   database.ref('/partners/').once('value')
   .then(function(snapshot){
-    let banana = []
+    let movies = []
     snapshot.forEach(function(childSnapshot) {
       var childKey = childSnapshot.key;
       var childData = childSnapshot.val();
       childSnapshot.forEach(function(d) {
-        banana.push(d.val())
+        movies.push(d.val())
+        // return movies;
+})
+// function getMoviesInDB()
+      })
+      for (var valor of movies) {
+        let image = valor.image;
+        let title = valor.title;
+        let discount = valor.discount;
+        $('#coupons').append(
+        `<div class="card" style="width: 18rem;">
+         <img class="card-img-top" src="${image}" alt="Card image cap">
+         <div class="card-body d-flex justify-content-between">
+           <h5 class="card-title">${title}</h5>
+           <h5 class="card-title">${discount}</h5>
+           <button type="button" class="btn btn-secondary">Reserve</button>
+         </div>
+        </div>
+        `);
+}
 
       })
-      })
-      return banana;
-    })
-}
+    }
